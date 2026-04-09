a3ca108884bc92fb4b7ed98edd69bdecdb70e117
N-Ayoub <nayoub@technorion.com>
2026-04-09 14:15:09 +0300

    adjust row selection functionality to only work when ops is checked out

--- src/components/transactions/TransactionsTab.tsx
+++ src/components/transactions/TransactionsTab.tsx
@@ -1067,7 +1067,7 @@ export function TransactionsTab({ activeCheckout, onClearPendingDefinition }: Tr
         highlightExpressions={highlightExpressions}
         searchHighlights={searchHighlights}
         onTagClick={handleTagClick}
-        onFlagDeadEnd={flagDeadEnd}
+        onFlagDeadEnd={!isReadOnly && !tagClickState?.showingAll && !tagClickState?.rulesetApplied ? flagDeadEnd : undefined}
         showAttributes={showAttributes}
         relaxedMode={relaxedMode}
         hiddenColumns={effectiveHiddenColumns}
